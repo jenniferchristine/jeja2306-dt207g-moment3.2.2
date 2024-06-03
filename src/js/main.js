310fc8250c3f99ef8870f95b06064272b0225d71
jenniferchristine <jeja2306@student.miun.se>
2024-06-03 16:35:05 +0200

    js for showing api results

--- src/js/main.js
+++ src/js/main.js
@@ -5,14 +5,52 @@ async function fetchData() {
 
     try {
         const response = await fetch(url);
-        if (!response.ok) {
+        if (!response.ok) { // felhantering
             throw new Error("Could not connect to API" + response.statusText);
         }
         const data = await response.json();
-        console.log(data);
+        return data; // returnerar från api
     } catch (error) {
         console.error("Could not fetch data", error);
+        throw error;
     }
-}
+};
+
+async function displayData() {
+    const resultDiv = document.getElementById("result");
+
+    try {
+        const data = await fetchData();
+        data.forEach( item => {
+            const workExperience = document.createElement("div");
+            workExperience.classList.add("workexperience");
+
+            workExperience.innerHTML = `
+            <h2>Arbetsplats: ${item.companyname}</h2>
+            <h3>Jobbtitel ${item.jobtitle}</h3>
+            <h4>Ort: ${item.location}</h4>
+            <p><span class="desc">"</span> ${item.description} <span class="desc">"</span>`
+    
+            const deleteBtn = document.createElement("button");
+            deleteBtn.classList.add("material-symbols-outlined");
+            deleteBtn.innerHTML = "delete";
+
+            deleteBtn.addEventListener('click', async (e) => {
+                try {
+                    const confirmation = confirm("OBS: Är du säker på att du vill radera denna?");
+
+                    if (confirmation) {
+                        // kalla på radera funktion
+                        resultDiv.removeChild(workExperience); // tar bort från sida
+                    }
+                } catch (error) {
+                    console.error("Error while deleting data", error);
+                }
+            });
+        });
+    } catch (error) {
+        console.error("Fault accured: ", error);
+    }
+};
 
 fetchData();
